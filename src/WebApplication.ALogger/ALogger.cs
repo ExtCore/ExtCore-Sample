df38e17800d0eca9718cac53fa3399fba7c34cde
USER001 F <USER001 F>
2016-06-19 23:42:56 +0900

    AddModuler
    WebApi and Swagger Sample
    Logging Chain
    
    Chack Road Sequence rule and Moduler Chains

---
+++ src/WebApplication.ALogger/ALogger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using ExtCore.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApplication.ALogger
+{
+    /// <summary>
+    /// read seaquence
+    /// CommonModuler[Logging] -> MainModuer[MVC,WEB API,DB,Swagger]
+    /// 
+    /// May be not using to ExtCore.Data Moduers, because ConfigureService Method on
+    /// Call service.AddEntityFramework or service.AddDBContext<IDBContext>()
+    /// 
+    /// Issue Autofac, LightInject, Catleproject.Core Can't Use IExtention
+    /// Do Not Make 'Ruby on Rails' CoC like rule.
+    /// </summary>
+    public class ALogger : IExtension
+    {
+        private IConfigurationRoot configurationRoot;
+
+        public string Name
+        {
+            get
+            {
+                return "ALogger";
+            }
+        }
+        public void SetConfigurationRoot(IConfigurationRoot configurationRoot)
+        {
+            this.configurationRoot = configurationRoot;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLocalization();
+        }
+
+        public void Configure(IApplicationBuilder applicationBuilder)
+        {
+            //loggerFactory.AddConsole();
+            //loggerFactory.AddDebug();
+        }
+
+        public void RegisterRoutes(IRouteBuilder routeBuilder)
+        {
+        }
+    }
+}
--- src/WebApplication/gulpfile.js
+++ src/WebApplication/gulpfile.js
@@ -2,11 +2,13 @@
 
 gulp.task(
   "copy-extensions", function (cb) {
+    //gulp.src(["../WebApplication.ALogger/bin/Debug/netstandard1.5/WebApplication.ALogger.dll"]).pipe(gulp.dest("Extensions"));
     gulp.src(["../WebApplication.ExtensionA/bin/Debug/netstandard1.5/WebApplication.ExtensionA.dll"]).pipe(gulp.dest("Extensions"));
     gulp.src(["../WebApplication.ExtensionB/bin/Debug/netstandard1.5/WebApplication.ExtensionB.dll"]).pipe(gulp.dest("Extensions"));
     gulp.src(["../WebApplication.ExtensionB.Data.Abstractions/bin/Debug/netstandard1.5/WebApplication.ExtensionB.Data.Abstractions.dll"]).pipe(gulp.dest("Extensions"));
     gulp.src(["../WebApplication.ExtensionB.Data.EntityFramework.Sqlite/bin/Debug/netstandard1.5/WebApplication.ExtensionB.Data.EntityFramework.Sqlite.dll"]).pipe(gulp.dest("Extensions"));
     gulp.src(["../WebApplication.ExtensionB.Data.Models/bin/Debug/netstandard1.5/WebApplication.ExtensionB.Data.Models.dll"]).pipe(gulp.dest("Extensions"));
+    gulp.src(["../WebApplication.ZSwagger/bin/Debug/netstandard1.5/WebApplication.ZSwagger.dll"]).pipe(gulp.dest("Extensions"));
     cb();
   }
 );
