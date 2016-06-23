58090f6f71b70626c7e213eab6994f4159c28364
USER001 F <USER001 F>
2016-06-24 01:25:55 +0900

    AddEntityFrameworkSample
    By InMemory DataBase

---
+++ src/WebApplication.BEntityFrameworkCore/BEntityFrameworkCore.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExtCore.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.BEntityFrameworkCore.Models;
+
+namespace WebApplication.BEntityFrameworkCore
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class BEntityFrameworkCore : IExtension
+    {
+        private IConfigurationRoot configurationRoot;
+
+        public string Name
+        {
+            get
+            {
+                return "BEntityFrameworkCore";
+            }
+        }
+        public void SetConfigurationRoot(IConfigurationRoot configurationRoot)
+        {
+            this.configurationRoot = configurationRoot;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddEntityFrameworkInMemoryDatabase();
+            services.AddDbContext<InMemoryContext>(option =>
+                option.UseInMemoryDatabase()
+            );
+            
+            /*using (var context = new InMemoryContext())
+            {
+                context.InMemorys.AddRange(new InMemory { Id = 1, Name = "Gobo" }, new InMemory { Id = 2, Name = "Monkey" }, new InMemory { Id = 3, Name = "Red" }, new InMemory { Id = 4, Name = "Wembley" }, new InMemory { Id = 5, Name = "Boober" }, new InMemory { Id = 6, Name = "Uncle Traveling Matt" });
+
+                context.SaveChangesAsync();
+            }*/
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+        }
+
+        public void RegisterRoutes(IRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapRoute(
+                name: "BEntityFrameworkCore",
+                template: "{controller=Home}/{action=Index}/{id?}");
+                //template: "extension-b", defaults: new { controller = "ExtensionB", action = "Index" });
+
+        }
+    }
+}
--- src/WebApplication/gulpfile.js
+++ src/WebApplication/gulpfile.js
@@ -3,6 +3,7 @@
 gulp.task(
   "copy-extensions", function (cb) {
     gulp.src(["../WebApplication.ALogger/bin/Debug/netstandard1.5/WebApplication.ALogger.dll"]).pipe(gulp.dest("Extensions"));
+    gulp.src(["../WebApplication.BEntityFrameworkCore/bin/Debug/netstandard1.5/WebApplication.BEntityFrameworkCore.dll"]).pipe(gulp.dest("Extensions"));
     gulp.src(["../WebApplication.ExtensionA/bin/Debug/netstandard1.5/WebApplication.ExtensionA.dll"]).pipe(gulp.dest("Extensions"));
     gulp.src(["../WebApplication.ExtensionB/bin/Debug/netstandard1.5/WebApplication.ExtensionB.dll"]).pipe(gulp.dest("Extensions"));
     gulp.src(["../WebApplication.ExtensionB.Data.Abstractions/bin/Debug/netstandard1.5/WebApplication.ExtensionB.Data.Abstractions.dll"]).pipe(gulp.dest("Extensions"));
