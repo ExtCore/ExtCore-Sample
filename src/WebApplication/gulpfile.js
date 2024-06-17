e53c6e09b8963017b4cc2994e34f87ad8c55f529
Dmitry Sikorsky <dmitry.a.sikorsky@gmail.com>
2024-06-17 11:57:14 +0300

    Updated to .NET 8 and ExtCore 9

--- src/WebApplication/gulpfile.js
+++ src/WebApplication/gulpfile.js
@@ -2,10 +2,10 @@
 
 gulp.task(
   "copy-extensions", function (cb) {
-    gulp.src(["../WebApplication.ExtensionA/bin/Debug/net6.0/WebApplication.ExtensionA.dll"]).pipe(gulp.dest("Extensions"));
-    gulp.src(["../WebApplication.ExtensionB/bin/Debug/net6.0/WebApplication.ExtensionB.dll"]).pipe(gulp.dest("Extensions"));
+    gulp.src(["../WebApplication.ExtensionA/bin/Debug/net8.0/WebApplication.ExtensionA.dll"]).pipe(gulp.dest("Extensions"));
+    gulp.src(["../WebApplication.ExtensionB/bin/Debug/net8.0/WebApplication.ExtensionB.dll"]).pipe(gulp.dest("Extensions"));
     gulp.src(["../WebApplication.ExtensionB.Data.Abstractions/bin/Debug/netstandard2.0/WebApplication.ExtensionB.Data.Abstractions.dll"]).pipe(gulp.dest("Extensions"));
-    gulp.src(["../WebApplication.ExtensionB.Data.EntityFramework.Sqlite/bin/Debug/net6.0/WebApplication.ExtensionB.Data.EntityFramework.Sqlite.dll"]).pipe(gulp.dest("Extensions"));
+    gulp.src(["../WebApplication.ExtensionB.Data.EntityFramework.Sqlite/bin/Debug/net8.0/WebApplication.ExtensionB.Data.EntityFramework.Sqlite.dll"]).pipe(gulp.dest("Extensions"));
     gulp.src(["../WebApplication.ExtensionB.Data.Entities/bin/Debug/netstandard2.0/WebApplication.ExtensionB.Data.Entities.dll"]).pipe(gulp.dest("Extensions"));
     cb();
   }
